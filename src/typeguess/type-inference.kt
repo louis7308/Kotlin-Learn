4bf4b6e79a5bdfe9268b32264a2c28f207db2e9b
전승원 <jswa7308@naver.com>
2022-03-25 08:12:17 +0900

    :sparkles: :: 함수, 단일표현식 함수

--- src/typeguess/type-inference.kt
+++ src/typeguess/type-inference.kt
@@ -13,4 +13,16 @@ fun main() {
 
     var g = true
     var h = 'a'
+
+    // 함수
+    println(add(5,6,7))
+    println(singleAddFunc(10, 20 , 30))
 }
+
+fun add(a: Int, b: Int, c: Int): Int { // 함수
+    return a + b + c;
+}
+
+//단일 표현식 함수
+fun singleAddFunc(a: Int,b: Int,c: Int) = a + b + c
+
